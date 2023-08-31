a3222973d06e130604a8e44be2893302b4367f6c
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2023-08-31 21:27:34 +0100

    working on metrics routes

--- src/routes/v1/metrics.router.ts
+++ src/routes/v1/metrics.router.ts
@@ -1,13 +1,23 @@
+import { AppService } from "@services/apps.service";
 import { MetricService } from "@services/metrics.service";
 import { Router } from "express";
 
+const appService = new AppService();
 const metricService = new MetricService();
 const router = Router();
 export default function metricsRouter() {
 	router.get("/", async (req, res) => {
-		const metrics = await metricService.getMetrics();
+		const apps = await appService.getUserApps((req.user as any).id);
+		const metrics = await metricService.getSummary(apps);
 
-		return res.json(metrics);
+		return res.json({ data: metrics });
+	});
+
+	router.get("/:appId", async (req, res) => {
+		const { appId } = req.params;
+		const metrics = await metricService.getAppSummary(appId);
+
+		return res.json({ data: metrics });
 	});
 
 	return router;
--- src/services/metrics.service.ts
+++ src/services/metrics.service.ts
@@ -1,6 +1,10 @@
+import { App } from "@entities/App";
 import { Metrics } from "@entities/Metrics";
 import { Database } from "@lib/database";
+import { format, subDays, subWeeks } from "date-fns";
 import { Repository } from "typeorm";
+
+const debug = require("debug")("app:metrics-service");
 
 export class MetricService {
 	private metricRepository: Repository<Metrics>;
@@ -13,6 +17,32 @@ export class MetricService {
 		if (!this.metricRepository) {
 			this.metricRepository = Database.datasource!.getRepository(Metrics);
 		}
+	}
+
+	public async getSummary(apps: App[]) {
+		// this.initialize();
+		const ids = apps.map((app) => app.id).join(",");
+
+		const query = Database.datasource!.createQueryRunner();
+		const date = format(new Date(), "yyyy-MM-dd 23:59:59");
+		const before = format(subWeeks(new Date(), 1), "yyyy-MM-dd 23:59:59");
+
+		const sql = `SELECT m.level, sum(m.weight) weight FROM metrics m WHERE (m.createdAt BETWEEN ? AND ?) AND m.appId IN (?) GROUP BY m.level`;
+
+		const data = await query.query(sql, [before, date, ids]);
+		return data;
+	}
+
+	public async getAppSummary(appId: string, timerange: string = "1d") {
+		const queryRunner = Database.datasource!.createQueryRunner();
+
+		const date = format(new Date(), "yyyy-MM-dd 23:59:59");
+		const before = format(subWeeks(new Date(), 1), "yyyy-MM-dd 23:59:59");
+		const data: { level: string; weight: number }[] = await queryRunner.query(
+			"SELECT level, sum(weight) weight, max(createdAt) createdAt FROM metrics WHERE (createdAt BETWEEN ? AND ?) AND appId = ? GROUP BY level, createdAt ORDER BY createdAt DESC",
+			[before, date, appId]
+		);
+		return data;
 	}
 
 	public async getMetrics() {
