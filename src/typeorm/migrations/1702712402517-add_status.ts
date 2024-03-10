49e5108ecd9b312493f2131385d43dc957c3c3a1
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-03-10 19:26:09 +0100

    fixing migrations

--- src/typeorm/migrations/1702712402517-add_status.ts
+++ src/typeorm/migrations/1702712402517-add_status.ts
@@ -4,11 +4,11 @@ export class AddStatus1702712402517 implements MigrationInterface {
     name = 'AddStatus1702712402517'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`team_member\` ADD \`status\` smallint NOT NULL`);
+        await queryRunner.query(`ALTER TABLE team_member ADD status smallint NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`team_member\` DROP COLUMN \`status\``);
+        await queryRunner.query(`ALTER TABLE team_member DROP COLUMN status`);
     }
 
 }
