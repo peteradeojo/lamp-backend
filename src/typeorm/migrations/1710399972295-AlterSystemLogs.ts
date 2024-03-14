f5cc0469379fa64eeb7b858f3e7243bb273ef56f
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-03-14 08:17:29 +0100

    fix: enum data type creation

--- src/typeorm/migrations/1710399972295-AlterSystemLogs.ts
+++ src/typeorm/migrations/1710399972295-AlterSystemLogs.ts
@@ -2,13 +2,6 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AlterSystemLogs1710399972295 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(`CREATE TYPE loglevel AS ENUM (
-            'info',
-            'error',
-            'warn',
-            'debug',
-            'critical',
-            'fatal');`);
 		await queryRunner.query(
 			`ALTER TABLE system_logs ADD from_system bit(1), ADD from_user bit(1), ADD context json NULL, ADD level loglevel NOT NULL;`
 		);
